bfad49adb7829a67bceb37133c70b843057b9038
Andy Jenkins <andy@gear11.com>
2014-08-24 21:19:29 -0400

    Tweak Exception declaration in main for compatibility with newer dependencies.

--- src/org/commoncrawl/examples/S3ReaderTest.java
+++ src/org/commoncrawl/examples/S3ReaderTest.java
@@ -16,7 +16,7 @@ import org.jets3t.service.model.S3Object;
  * @author Stephen Merity (Smerity)
  */
 public class S3ReaderTest {
-	public static void main(String[] args) throws IOException, S3ServiceException {
+	public static void main(String[] args) throws Exception {
 		// We're accessing a publicly available bucket so don't need to fill in our credentials
 		S3Service s3s = new RestS3Service(null);
 		
